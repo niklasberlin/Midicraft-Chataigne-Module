84f06f84b545214312d46646188cfc9b567873ab
niklasberlin <github@niklas-aumueller.de>
2023-04-29 20:02:47 +0200

    updated code to match current midicraft FW

--- midicraft.js
+++ midicraft.js
@@ -12,18 +12,21 @@ function set_led(channel, note, animation, color){
 		if (animation==1){
 			local.sendNoteOn(channel, note, 126); //velocity 126 for static dark in color defined in controller
 			return;
-		}else if (animation==15){
+		}else if (animation==17){
 			local.sendNoteOn(channel, note, 127); //velocity 127 for static bright in color defined in controller
 			return;
-		}else if (animation==29){
-			local.sendNoteOn(channel, note, 124); //velocity 124 for flashing bright/dark in color defined in controller
+		}else if (animation==33){
+			local.sendNoteOn(channel, note, 124); //velocity 127 for static complementary color
 			return;
-		}else if (animation==43){
-			local.sendNoteOn(channel, note, 125); //velocity 125 for flashing dark/off in color defined in controller
+		}else if (animation==49){
+			local.sendNoteOn(channel, note, 125); //velocity 124 for flashing bright/dark in color defined in controller
+			return;
+		}else if (animation==65){
+			local.sendNoteOn(channel, note, 123); //velocity 123 for feedback and color as defined in controller
 			return;
 		}
-	}else{
-		local.sendNoteOn(channel, note, (animation+color)); //velocity 125 for flashing dark/off in color defined in controller
+	}else{ //compute velocity for all feedbacks where we want to control the color
+		local.sendNoteOn(channel, note, (animation+color));
 	}
 
 }
